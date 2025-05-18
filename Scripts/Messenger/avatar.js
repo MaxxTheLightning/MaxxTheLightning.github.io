72104411ce377041c794012ea839abff08e79d2e
MaxxTheLightning <maksim.kryukov2009@gmail.com>
2025-05-18 12:38:14 +0300

    Updating for my landing page

--- Scripts/Messenger/avatar.js
+++
@@ -1,7 +0,0 @@
-// Made by MaxxTheLightning, 2025
-const image = document.querySelector("img"),
-input = document.querySelector("input");
-
-input.addEventListener("change", () => {
-    image.src = URL.createObjectURL(input.files[0]);
-})
