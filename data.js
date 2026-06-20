3b7e7215c0645375ee3acd5381c97de046e245c2
MaxxTheLightning <maksim.kryukov2009@gmail.com>
2026-06-20 15:02:01 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const data = `
+{
+  "tracks": [
+    {
+      "track_name": "Galvanize",
+      "author": "The Chemical Brothers",
+      "track_cover_dir": "Media/Covers/Galvanize.jpg",
+      "track_dir": "Media/Tracks/Galvanize.mp3"
+    },
+    {
+      "track_name": "Keep Up",
+      "author": "Odetari",
+      "track_cover_dir": "Media/Covers/Keep_Up.jpg",
+      "track_dir": "Media/Tracks/Keep_Up.mp3"
+    },
+    {
+      "track_name": "Dracula",
+      "author": "Tame Impala",
+      "track_cover_dir": "Media/Covers/Dracula.jpg",
+      "track_dir": "Media/Tracks/Dracula.mp3"
+    }
+  ],
+  "clips": [
+    {
+      "clip_name": "Keep Up",
+      "author": "Odetari",
+      "clip_dir": "Media/Clips/Keep_Up.mp4"
+    }
+  ]
+}`
